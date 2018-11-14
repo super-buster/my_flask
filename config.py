bebb66d6d3bb16f8b24064b005f3fc1691fd63a7
Kaori <424295833@qq.com>
2018-11-14 18:42:45 +0800

    add email config into config.py

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ basedir=os.path.abspath(os.path.dirname(__file__)) #获得当前文件（比如�
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MAIL_SERVER=os.environ.get('MAIL_SERVER','smtp.google.com')
+    MAIL_PORT=int(os.environ.get('MAIL_PORT','587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS=['424295833iou@gmail.com']
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
