d8f8509dbee051531862891f7574a5c244a45fea
Kaori <424295833@qq.com>
2018-11-30 23:56:07 +0800

    pagination in the user profile view function

--- app/routes.py
+++ app/routes.py
@@ -71,14 +71,13 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    user=User.query.filter_by(username=username).first()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    if user is None:
-        abort(404)
-    return render_template('user.html',user=user,posts=posts)
+    user=User.query.filter_by(username=username).first_or_404()
+    page=request.args.get('page',1,type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url=url_for('user',username=username,page=posts.next_num) if posts.has_next else None
+    prev_url=url_for('user',username=username,page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html',user=user,posts=posts.items,next_url=next_url,prev_url=prev_url)
 
 @app.route('/edit_profile',methods=['GET','POST'])
 @login_required
@@ -130,7 +129,7 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    page=request.args.get('page')
+    page=request.args.get('page',1,type=int)
     posts=Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'],False)
     next_url = url_for('index', page=posts.next_num) if posts.has_next else None
