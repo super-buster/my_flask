5c9aac31b4073c92d6fddecbedb7b3cc159cec08
Kaori <424295833@qq.com>
2018-12-01 23:42:25 +0800

    modify config.py , ADMINS changed

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     MAIL_SERVER=os.environ.get('MAIL_SERVER')
     MAIL_PORT=int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS=['424295833iou@gmail.com']
+    ADMINS=['424295833@qq.com']
     POSTS_PER_PAGE=25
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
