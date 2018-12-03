73d63ffdc08263c032fd7c006f31343ddf4de14f
Kaori <424295833@qq.com>
2018-12-03 21:58:11 +0800

    change send information

--- app/email.py
+++ app/email.py
@@ -9,11 +9,11 @@ def send_email(subject, sender, recipients, text_body, html_body):
     mail.send(msg)
 
 def send_password_reset_email(user):
-    token=user.get_reset_password_token()
-    send_email('[Microblog] Reset your Password',
+    token = user.get_reset_password_token()
+    send_email('Reset Your Password',
                sender=app.config['MAIL_USERNAME'],
-               recipients=app.config[user.email],
+               recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
-                                         user=user,token=token),
+                                         user=user, token=token),
                html_body=render_template('email/reset_password.html',
-                                         user=user,token=token))
+                                         user=user, token=token))
