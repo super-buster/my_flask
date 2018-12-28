be526a947cfae71617dd945c665d018b7be3a132
Kaori <424295833@qq.com>
2018-12-28 15:02:59 +0800

    base.html change(index-->main.index , login-->auth.login ...)

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 manage=Manager(app)
 login=LoginManager(app)
-login.login_view='login'
+login.login_view='auth.login'
 login.login_message = _l('Please log in to access this page.')
 mail=Mail(app)
 moment=Moment(app)
@@ -31,6 +31,9 @@ app.register_blueprint(errors_bp)
 
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
