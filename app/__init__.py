d63a4cf7015362e5e44e78da336ea62b1bf32635
Kaori <424295833@qq.com>
2018-12-31 00:15:39 +0800

    Application factory function.

--- app/__init__.py
+++ app/__init__.py
@@ -13,60 +13,71 @@ import logging
 from logging.handlers import SMTPHandler ,RotatingFileHandler
 import os
 
-app = Flask(__name__)
-app.config.from_object(Config)
-bootstrap=Bootstrap(app)
-db=SQLAlchemy(app)
-migrate=Migrate(app,db)
-manage=Manager(app)
-login=LoginManager(app)
+bootstrap=Bootstrap()
+db=SQLAlchemy()
+migrate=Migrate()
+manage=Manager()
+login=LoginManager()
 login.login_view='auth.login'
 login.login_message = _l('Please log in to access this page.')
-mail=Mail(app)
-moment=Moment(app)
-babel=Babel(app)
+mail=Mail()
+moment=Moment()
+babel=Babel()
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
 
-from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp, url_prefix='/auth')
+def creat_app(config_clas=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from app.main import bp as main_bp
-app.register_blueprint(main_bp)
+    db.init_app(app)
+    migrate.init_app(app,db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
 
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr=app.config['MAIL_USERNAME'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                       backupCount=10)
-    #设置时间，日志级别，用户输出的信息，调用日志输出函数的模块的文件名，所在的代码行
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
+    if not app.debug:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr=app.config['MAIL_USERNAME'],
+                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                           backupCount=10)
+        #设置时间，日志级别，用户输出的信息，调用日志输出函数的模块的文件名，所在的代码行
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+    return app
 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-from app import routes,models,forms
+from app import models
