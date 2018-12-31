65ebf329b67a286e33b7969c73c61e53a85927e4
Kaori <424295833@qq.com>
2018-12-31 20:23:22 +0800

    register custom application commands. main application module refactored.

--- flasky.py
+++ flasky.py
@@ -1,10 +1,10 @@
-from app import app,db,manage
+from app import db,creat_app,cli
 from app.models import User,Post
-from flask_script import  Shell
-from app import cli
+
+
+app= creat_app()
+cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Post=Post)
-manage.add_command("shell",Shell(make_context=make_shell_context))
-
