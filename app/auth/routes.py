ab3587f9c7fe8832f53ff116883de88a111cb0bf
Kaori <424295833@qq.com>
2019-01-02 00:20:14 +0800

    update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -8,26 +8,22 @@ from app.auth.forms import LoginForm, RegistrationForm,ResetPasswordRequestForm,
 from app.models import User
 from app.auth.email import send_password_reset_email
 
-@bp.route('/login',methods=['GET','POST'])
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    #先确保没有登录
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
-    form=LoginForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        #查找数据库里面的符合条件的第一个对象
-        user=User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.verify_password(form.password.data):
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
             flash(_('Invalid username or password'))
             return redirect(url_for('auth.login'))
-        #记为登录状态
-        login_user(user,remember=form.remember_me.data)
-        next_page=request.args.get('next')
-        #检查netloc属性是否被设置,确保安全
-        if not next_page or url_parse(next_page).netloc!='':
-            next_page=url_for('main.index')
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.index')
         return redirect(next_page)
-    return render_template('auth/login.html',title='Sign in',form=form)
+    return render_template('auth/login.html', title=_('Sign In'), form=form)
 
 @bp.route('/logout')
 def logout():
