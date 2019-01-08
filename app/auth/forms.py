7d12739c265b5d40f56e0d27fcb12323825997cc
Kaori <424295833@qq.com>
2019-01-08 16:10:53 +0800

    delete __init__()

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -38,10 +38,6 @@ class EditProfileForm(FlaskForm):
     about_me=TextAreaField(_l('About me'),validators=[Length(0,1024)])
     submit=SubmitField(_l('Submit'))
 
-    #继承Flask的__init__方法
-    def __init__(self,original_username,*args,**kwargs):
-        super(EditProfileForm, self).__init__(*args,**kwargs)
-        self.original_username=original_username
 
     def validate_username(self,username):
         if username.data!=self.original_username:
