dffa0b9e7a7c0761a103667838606a0d174fb00a
Kaori <424295833@qq.com>
2019-01-12 10:08:07 +0800

    fix a bug(check_password --> verify_password)

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -15,7 +15,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
+        if user is None or not user.verify_password(form.password.data):
             flash(_('Invalid username or password'))
             return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
