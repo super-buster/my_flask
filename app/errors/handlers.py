e92fcc29241dbb21698a4521301d2859fc796e64
Kaori <424295833@qq.com>
2019-05-14 17:05:33 +0800

    decorators add(for verify admin)

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,6 +3,9 @@ from flask import render_template
 from app import db
 from app.errors import bp
 
+@bp.app_errorhandler(403)
+def forbidden(error):
+    return render_template('errors/403.html'),403
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
